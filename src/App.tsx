a085062d070c1ff93afa03780dcc02bf479aed8e
aslan-kollontai <212768491+aslan-kollontai@users.noreply.github.com>
2025-10-15 16:43:01 +0400

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import Layout from "./components/Layout";
-import Hero from "./components/Hero";
-import Services from "./components/Services";
-import Divider from "./components/Divider";
-import Benefits from "./components/Benefits";
-import Testimonials from "./components/Testimonials";
-import Contact from "./components/Contact";
+import Layout from "@components/Layout";
+import Hero from "@components/Hero";
+import Services from "@components/Services";
+import Divider from "@components/Divider";
+import Benefits from "@components/Benefits";
+import Testimonials from "@components/Testimonials";
+import Contact from "@components/Contact";
 
 function App() {
   useEffect(() => {
