76810e3679ebd4513bfeff1013b5644f0e99d1c8
A. <212768491+aslan-kollontai@users.noreply.github.com>
2025-10-15 22:36:55 +0400

    .

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,6 @@
-import NavBar from "@components/NavBar";
-import Footer from "@components/Footer";
-import Divider from "@components/Divider";
+import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
+import Divider from "@/components/Divider";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
